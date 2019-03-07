4cf19c464c6f1e2d46eaeb77dd0d6a36a4837dc4
alanoud-GA <alanoud.alrasheed@generalassemb.ly>
2019-03-07 13:41:55 +0300

    solution is up

--- starter.js
+++ starter.js
@@ -1,1 +1,24 @@
-console.log("starter.js is connected")
+var checkWin = 0
+
+$(".zone").on("mouseenter", function (event) {
+    $(event.target).css("background-color", "green");
+});
+
+$(".zone").on("mouseleave", function (event) {
+    $(event.target).css("background-color", "white");
+});
+
+$(".zone").on("click", function (event) {
+    $(event.target).addClass("green")
+    $(event.target).off("mouseleave");
+    checkWin = 0;
+    $(".zone").each(function () {
+        if ($(this).hasClass("green")) {
+            checkWin++;
+        }
+    })
+
+    if (checkWin === 4) {
+        console.log("congrats");
+    }
+});
